bc21bbb05ed7ef05c400f23f97ab5d3f3c3e8912
William Wainwright <51410426+wjwainwright@users.noreply.github.com>
2020-04-09 14:38:30 -0400

    Image stacking
    Added method for subtracting and then stacking images to see the trajectory of the meteor in one image

--- processData.py
+++ processData.py
@@ -8,7 +8,7 @@ def splitAVI(dates,src='data/avi/',dest='data/img/',status=False):
     for date in dates:
         if not os.path.exists(f"{src}{date}"):
             print(f"{src}{date} directory does not exist")
-            return
+            continue
         
         if not os.path.exists(f'{dest}{date}'):
             os.makedirs(f'{dest}{date}')
@@ -54,22 +54,59 @@ def getList(start,end):
     
 
 
+def processDates(dates,src='data/img/',dest='data/subtracted/'):
+    import cv2
+    import os
+    import numpy as np
+    
+    for date in dates:
+        images = loadImg(date,src)
+        
+        if not os.path.exists(f'{dest}{date}/'):
+            os.mkdir(f'{dest}{date}')
+        
+        #Images is an array of observations, each observation is an array of actual images from that observation
+        for obs in images:
+            
+            if not os.path.exists(f'{dest}{date}/{obs[0]}'):
+                os.mkdir(f'{dest}{date}/{obs[0]}')
+            
+            #Make the numbr of images correct for stacking purposes
+            #Usually the first few frames contains no information anyway
+            #The first index in the list is actually a string with the file name,
+            #So len(obs)%5 should be 1 if there are the correct number of images
+            while not len(obs)%5 == 1:
+                obs.pop(1)
+            
+            #Subtract the images in intervals of 5
+            subList = []
+            for i in range(1,len(obs),5):
+                sub = cv2.subtract(obs[i+4],obs[i])
+                subList.append(sub)
+                cv2.imwrite(f'{dest}{date}/{obs[0]}/{i}_{i+4}.png',sub)
+            
+            #Stack the subtracted images
+            stacked = subList[0]
+            for i in range(1,len(subList)):
+                stacked = cv2.add(stacked,subList[i])
+            cv2.imwrite(f'{dest}{date}/{obs[0]}/_stacked.png',stacked)
+
+
 
 
 def loadImg(date,src='data/img/'):
     import os
     import cv2
     
-    
-    images = dict()
     folder = f"{src}{date}/"
-    iList = []
+    images = []
     
     for file in os.listdir(folder):
+        iList = [file]
         for fn in os.listdir(f'{folder}{file}/'):
             iList.append(cv2.imread(f"{folder}{file}/{fn}"))
+        images.append(iList)
     
-    images[date] = iList
     
     return images
         
